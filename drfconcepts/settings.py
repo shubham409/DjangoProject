3d7f4cfe3c1456a9493c0e144bbde84ba6705196
shubham409 <sm091274@gmail.com>
2022-02-14 11:55:00 +0530

    updating settings.py

--- drfconcepts/settings.py
+++ drfconcepts/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'customapi',
     'allmodels',
+    'relationships',
+    'validators',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
