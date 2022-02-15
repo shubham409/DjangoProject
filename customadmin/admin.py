20343ee38ddb4f8b498724c25ce43c8f5e10566c
shubham409 <sm091274@gmail.com>
2022-02-15 16:50:34 +0530

    For changing table name in admin and displaying multiple column in admin

--- customadmin/admin.py
+++ customadmin/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from customadmin.models import Collegs
+
 # Register your models here.
+
+class CustomAdmin(admin.ModelAdmin):
+    list_display  = ('name','total_students','branches','location','university')
+
+
+
+admin.site.register(Collegs,CustomAdmin)
--- customadmin/models.py
+++ customadmin/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Collegs(models.Model):
+    name = models.CharField(max_length=200)
+    total_students = models.IntegerField(default=1000)
+    branches = models.IntegerField(default=6)
+    location = models.CharField(max_length=100 ,default="New Delhi")
+    university = models.CharField(max_length=200, default = "IPU")
+
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "collegs"
+        verbose_name="College Table"
