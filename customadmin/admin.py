34d9d1d8ae4ade8fb0060a3194ee88164c6ce333
shubham409 <sm091274@gmail.com>
2022-02-16 11:51:59 +0530

    Custom Filter Created using classes

--- customadmin/admin.py
+++ customadmin/admin.py
@@ -6,6 +6,49 @@ from customadmin.models import Collegs
 from django.db.models import Avg
 # Register your models here.
 
+from django.contrib.admin import ModelAdmin, SimpleListFilter
+
+
+class CustomFilter(SimpleListFilter):
+    title = "Students Count"  # a label for our filter
+    parameter_name = "anything"  # you can put anything here
+
+    def lookups(self, request, model_admin):
+        # This is where you create filter options; we have two:
+        return [
+            ("above_400", "Above 400"),
+            ("below_400", "Below 400"),
+        ]
+
+    def queryset(self, request, queryset):
+        # This is where you process parameters selected by use via filter options:
+        if self.value() == "above_400":
+            # Get websites that have at least one page.
+            return queryset.filter(total_students__gte=400)
+        if self.value():
+            # Get websites that don't have any pages.
+            return queryset.filter(total_students__lte=400)
+
+
+
+class CertifiedFilter(SimpleListFilter):
+    title = "Certification"
+    parameter_name = "anything"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("Certified", "Certified"),
+            ("Not_Certified", "Not Certified"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "Certified":
+            return queryset.filter(certified=1)
+        if self.value():
+            return queryset.filter(certified=0)
+
+
+
 class CustomAdmin(admin.ModelAdmin):
     # for multiple column 
     list_display  = ('name','good_or_not','total_students','branches','location','university','ranking','student_strength','is_certified')
@@ -55,11 +98,10 @@ class CustomAdmin(admin.ModelAdmin):
     
 
     # for filters
-    list_filter = ('name','branches')
+    list_filter = ('name','branches',CertifiedFilter,CustomFilter,)
 
     # for searchin values use any lookups here
     search_fields=('name__icontains',)
-
 
 
 admin.site.register(Collegs,CustomAdmin)
